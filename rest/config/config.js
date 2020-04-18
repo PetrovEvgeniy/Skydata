86b66d3614d16d97e8718d478b8140819f91d73f
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2020-04-18 21:47:12 +0300

    added mongodb connection string

--- rest/config/config.js
+++ rest/config/config.js
@@ -7,8 +7,8 @@ const config = {
         authCookieName: 'x-auth-token'
     },
     production: {
-        port: process.env.PORT || 9999,
-        dbURL: 'mongodb://localhost:27017/skydata-db',
+        port: process.env.PORT,
+        dbURL: 'mongodb+srv://admin-skydata:2MyqBonq8GEutr1v@skydata-project-cluster-gm2gy.mongodb.net/test?retryWrites=true&w=majority',
         authCookieName: 'x-auth-token'
     }
 };
